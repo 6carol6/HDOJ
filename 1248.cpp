73869b6d878687207ad11f3aa51b9440b9560ba1
6carol6 <mszxw999@gmail.com>
2015-08-15 09:48:53 +0800

    CompletePack

---
+++ 1248.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <memory.h>
+using namespace std;
+
+const int MAXN = 10001;
+int dp[MAXN];
+int value[3] = {150, 200, 350};
+int nValue;
+
+int max(int a, int b){
+	return a>b?a:b;
+}
+
+void CompletePack(int cost, int weight){
+	for(int i = cost; i <= nValue; i++){
+		dp[i] = max(dp[i], dp[i-cost]+weight);
+	}
+}
+
+void work(){
+	int N;
+	cin >> N;
+	nValue = N;
+	memset(dp, 0, sizeof(dp));
+	for(int i = 0; i < 3; i++){
+		CompletePack(value[i], value[i]);
+	}
+	cout << nValue - dp[nValue] << endl;
+}
+
+int main(){
+	int T;
+	cin >> T;
+	while(T--) work();
+	return 0;
+}
